5d2f8aa2ec0e44d3ef41bb65c8b4ecd81fa1ef7e
3007akshay <chaturvediakshay667@gmail.com>
2024-12-07 16:47:21 +0530

    Multiple_routehandler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
+
+// app.use("/hello2", (req, res) => {
+//   res.send("Hello Hello");
+// });
+
+// app.use("/hello1", (req, res) => {
+//   res.send("Hello Hello 2");
+// });
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("hanler 1");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("handler 2");
+    next();
+    res.send("Hello");
+  },
+  (req, res) => {
+    console.log("handler 3");
+    res.send("Logged in");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is live on port 3000");
